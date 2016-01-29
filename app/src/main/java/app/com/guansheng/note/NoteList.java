42ddc57ddebeea7e5845f9170a9c71090c0b5bdc
Willy Chen <st40809@gmail.com>
2016-01-29 23:37:01 +0000

    Set ArrayAdapter (java)

--- app/src/main/java/app/com/guansheng/note/NoteList.java
+++ app/src/main/java/app/com/guansheng/note/NoteList.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class NoteList extends AppCompatActivity implements AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener{
+    String[] note={
+            "按一下編輯記事",
+            "長按清除記事",
+            "",
+            "",
+            "",
+            ""
+    };
 
     ListView listView;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,8 @@ public class NoteList extends AppCompatActivity implements AdapterView.OnItemCli
         setContentView(R.layout.note_list);
 
         listView = (ListView) findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,note);
+        listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(this);
         listView.setOnItemLongClickListener(this);
     }
