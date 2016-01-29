83fbeffd0237d09256b2a22c5285cf25c689f453
Willy Chen <st40809@gmail.com>
2016-01-29 23:57:07 +0000

    Get Intent (java)

--- app/src/main/java/app/com/guansheng/note/NoteDetail.java
+++ app/src/main/java/app/com/guansheng/note/NoteDetail.java
@@ -1,14 +1,21 @@
 package app.com.guansheng.note;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class NoteDetail extends AppCompatActivity {
+    int no;
+    String note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_detail);
+
+        Intent intent = getIntent();
+        no = intent.getIntExtra("no", 0);
+        note = intent.getStringExtra("note");
     }
 }
