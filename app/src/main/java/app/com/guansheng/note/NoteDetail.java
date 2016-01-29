c0a9fb853615c4f4099f067e25f2068edf002f85
Willy Chen <st40809@gmail.com>
2016-01-29 23:54:52 +0000

    NoteDetail basic (java)

--- app/src/main/java/app/com/guansheng/note/NoteDetail.java
+++ app/src/main/java/app/com/guansheng/note/NoteDetail.java
@@ -1,5 +1,14 @@
 package app.com.guansheng.note;
 
 
-public class NoteDetail {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NoteDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_detail);
+    }
 }
